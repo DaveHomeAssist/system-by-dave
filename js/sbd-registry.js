c49617088fdfbb5f7a9d1c3060c4e7ff05c10d31
DaveHomeAssist <infodave53@Gmail.com>
2026-09-22 15:11:01 -0400

    Phase 2: register extracted AV Suite assets for offline caching

--- js/sbd-registry.js
+++ js/sbd-registry.js
@@ -109,6 +109,10 @@
   var BASE_ASSETS=[
     './av-suite.html',
     './av-suite-worker.js',
+    './css/av-suite.css',
+    './js/av-suite/theme-bootstrap.js',
+    './js/av-suite/modal-controller.js',
+    './js/av-suite/app.js',
     './js/sbd-registry.js',
     './js/av-suite-context.js',
     './js/sbd-nav.js',
